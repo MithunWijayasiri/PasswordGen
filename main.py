b2280745740a904b27ba20a8bc697cfaf4022726
MithunWijayasiri <mithunedu1@gmail.com>
2022-10-09 23:51:31 +0530

    Added "logical" conditions

--- main.py
+++ main.py
@@ -18,13 +18,28 @@ output1 = lower+upper+num
 output2 = lower+upper+num+symbols
 
 #make random module to generate the password
-temp1 = random.sample(output1, length)
-temp2 = random.sample(output2, length)
-
-#join all characters together
-password1 ="".join(temp1)
-password2 ="".join(temp2)
-
-print('\nPassword Generated Successfully.')
-print('> Simple: '+password1)
-print('> Strong: '+password2)
+if length < 1:
+    print('Invalid Input!')
+    
+elif length < 8:
+    temp1 = random.sample(output1, length)
+    temp2 = random.sample(output2, length)
+    
+    #join all characters together
+    password1 ="".join(temp1)
+    password2 ="".join(temp2)
+    print('\nPassword generated but not recommended to use this password.')
+    print('Tip*Input a number Greater Than 8.')
+    print('\n> Output 1: '+password1)
+    print('> Output 2: '+password2)
+    
+else:
+    temp1 = random.sample(output1, length)
+    temp2 = random.sample(output2, length)
+    
+    #join all characters together
+    password1 ="".join(temp1)
+    password2 ="".join(temp2)
+    print('\nPassword Generated Successfully.')
+    print('> Simple: '+password1)
+    print('> Strong: '+password2)
