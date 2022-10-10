c522b7ea4401348deca3ca794e7bc9eaa3c3fba7
MithunWijayasiri <mithunedu1@gmail.com>
2022-10-10 16:49:57 +0530

    Optimized the code

--- main.py
+++ main.py
@@ -21,18 +21,6 @@ output2 = lower+upper+num+symbols
 if length < 1:
     print('Invalid Input!')
     
-elif length < 8:
-    temp1 = random.sample(output1, length)
-    temp2 = random.sample(output2, length)
-    
-    #join all characters together
-    password1 ="".join(temp1)
-    password2 ="".join(temp2)
-    print('\nPassword generated but not recommended to use this password.')
-    print('Tip*Input a number Greater Than 8.')
-    print('\n> Output 1: '+password1)
-    print('> Output 2: '+password2)
-    
 else:
     temp1 = random.sample(output1, length)
     temp2 = random.sample(output2, length)
@@ -40,6 +28,14 @@ else:
     #join all characters together
     password1 ="".join(temp1)
     password2 ="".join(temp2)
-    print('\nPassword Generated Successfully.')
-    print('> Simple: '+password1)
-    print('> Strong: '+password2)
+    
+    if length < 8:
+        print('\nPassword generated but not recommended to use this password.')
+        print('Input a number Greater Than 8.')
+        print('\n> Output 1: '+password1)
+        print('> Output 2: '+password2)
+        
+    else:
+        print('\nPassword Generated Successfully.')
+        print('> Simple: '+password1)
+        print('> Strong: '+password2)
